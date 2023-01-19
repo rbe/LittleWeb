945a81d4181f01124925fcd8e1618c3f00ca009f
Ralf Bensmann <ralf@art-of-coding.eu>
2023-01-19 14:57:06 +0100

    Asciidoc, PlantUML

--- cgi-bin/doc_renderer/doc_renderer.rb
+++ cgi-bin/doc_renderer/doc_renderer.rb
@@ -13,10 +13,11 @@ module DocRenderer
       @std_document_attrs = {
         'license' => 'All rights reserved. Use is subject to license terms.',
         'docinfo' => 'shared',
-        '!webfonts' => '',
-        'linkcss' => '',
-        'copycss' => '',
+        '!webfonts' => nil,
+        'linkcss' => true,
+        'copycss' => true,
         'icons' => 'font',
+        '!iconfont-remote' => nil,
         'toclevels' => 3,
         'kroki-server-url' => Constants::KROKI_URL,
         'kroki-default-options' => 'inline',
@@ -28,24 +29,39 @@ module DocRenderer
     # @param [String] file
     def render(file)
       # input = File.read(file, encoding: 'utf-8')
-      adoc_to_html(file)
+      absolute_document_dir, output_file = file_coordinates(file)
+      adoc_to_html(file) unless File.exist? "#{absolute_document_dir}/#{output_file}"
     end
 
     private
 
+    # @param [String] file
     def adoc_to_html(file)
+      opts = asciidoctor_options(file)
+      Asciidoctor.convert_file file, opts
+      File.read(opts[:to_file], encoding: 'utf-8')
+    end
+
+    # @param [String] file
+    # @return [Hash<String, String>]
+    def asciidoctor_options(file)
+      absolute_document_dir, output_file = file_coordinates(file)
+      {
+        doctype: 'book',
+        backend: 'html5',
+        safe: 'unsafe',
+        base_dir: absolute_document_dir,
+        to_dir: absolute_document_dir,
+        to_file: output_file,
+        attributes: @std_document_attrs
+      }
+    end
+
+    def file_coordinates(file)
       absolute_document_dir = File.dirname File.absolute_path(file)
       pure_file_name = File.basename file, '.adoc'
       output_file = "#{absolute_document_dir}/#{pure_file_name}.html"
-      Asciidoctor.convert_file file,
-                               doctype: 'book',
-                               backend: 'html5',
-                               safe: 'unsafe',
-                               base_dir: absolute_document_dir,
-                               to_dir: absolute_document_dir,
-                               #to_file: output_file,
-                               attributes: @std_document_attrs
-      File.read(output_file, encoding: 'utf-8')
+      [absolute_document_dir, output_file]
     end
   end
 end
