4b8029c472445ba4609dd890010310cf306c58b7
Ralf Bensmann <ralf@art-of-coding.eu>
2023-07-24 19:09:50 +0200

    Fixed instantiate controller, catch error

--- src/secure_access/dispatcher/front_dispatcher.rb
+++ src/secure_access/dispatcher/front_dispatcher.rb
@@ -24,29 +24,32 @@ module SecureAccess
 
       # Dispatch
       def dispatch
-        # TODO: Generify
-        controller = case full_request_uri
-                     when %r{^.*/registration.*$}
-                       Controller::OtpRegistrationController.new(@request, @response)
-                     when %r{^.*/access_request$}
-                       Controller::AccessRequestController.new(@request, @response)
-                     when %r{^.*/exchange.*$} # \?.+token=.+&hash=.+
-                       Controller::ExchangeController.new(@request, @response)
-                     when %r{^.*/debug_.+}
-                       Controller::DebugTokenController.new(@request, @response)
-                     when %r{^.*/$}
-                       Controller::IndexController.new(@request, @response)
-                     else
-                       Controller::SimpleProxyController.new(@request, @response)
-                     end
+        controller = select_controller.new(@request, @response)
         return @response.server_error 'Not processable' unless controller.processable?
 
         controller.process
-        # rescue StandardError => e
-        # handle_error(e)
+      rescue StandardError => e
+        @response.server_error "#{e.cause}, #{e.detailed_message}, #{e.backtrace&.join('\n').to_s}"
       end
 
       private
+
+      def select_controller
+        case full_request_uri
+        when %r{^.*/registration.*$}
+          Controller::OtpRegistrationController
+        when %r{^.*/access_request$}
+          Controller::AccessRequestController
+        when %r{^.*/exchange.*$} # \?.+token=.+&hash=.+
+          Controller::ExchangeController
+        when %r{^.*/debug_.+}
+          Controller::DebugTokenController
+        when %r{^.*/$}
+          Controller::IndexController
+        else
+          Controller::SimpleProxyController
+        end
+      end
 
       def full_request_uri
         uri = @request.request_uri
